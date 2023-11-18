e72f5f8609cf436a8d80a6362ea04f2f83261e91
Krishna <krishnavarela.a@gmail.com>
2023-11-18 11:16:35 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+// import { ProductModule } from './modules/products/products.module';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
